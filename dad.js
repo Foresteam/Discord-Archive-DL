53a01a350fa8d62ace38247fa4b4d4e616da6bf8
Foresteam <heler1141@gmail.com>
2022-01-23 16:59:39 +0300

    Now copies automatically

--- dad.js
+++ dad.js
@@ -1,8 +1,16 @@
+try { document.querySelector('#copirovalka').remove(); } catch { }
+var copirovalka = document.createElement('input');
+copirovalka.setAttribute('id', 'copirovalka');
+copirovalka.type = 'text';
+document.body.appendChild(copirovalka);
+
 let msgId = '934793791121031240';
-console.log(
-    'wget ' +
+copirovalka.value = 'wget ' +
     Array.prototype.slice.call(document.querySelector(`#message-accessories-${msgId}`).children)
         .map(v => v.querySelector('a').getAttribute('href'))
         .map(v => `"${v}"`)
-        .join(' ')
-);
+        .join(' ');
+copirovalka.focus();
+copirovalka.select();
+document.execCommand('copy');
+alert('Copied!');
